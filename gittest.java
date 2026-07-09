1005279204666d52dc9cf853052988fbcf2b9d5c
Kevin <kevindasilvalima2008@gmail.com>
2026-07-09 13:46:49 -0300

    Update gittest.java

--- gittest.java
+++ gittest.java
@@ -2,6 +2,7 @@ public class gittest{
     public static void main(String[] args) {
         System.out.println("Testando");
         ola();
+        ola():
     }
     
     public static void ola (){
