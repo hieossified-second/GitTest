4022177b0c9a4e670c19866194092ffefc096ed1
  <kevindasilvalima2008@gmail.com>
2026-07-09 13:52:36 -0300

    ola

--- gittest.java
+++ gittest.java
@@ -2,7 +2,7 @@ public class gittest{
     public static void main(String[] args) {
         System.out.println("Testando");
         ola();
-        ola():
+        ola();
     }
     
     public static void ola (){
